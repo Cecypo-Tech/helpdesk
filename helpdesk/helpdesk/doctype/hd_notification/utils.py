d59524c82bb5f334e32c4eac1add76a18bd95557
KushX <kushal@Kushal.localdomain>
2026-06-07 03:32:14 +0300

    feat(notifications): add Task type and single-notification clear

--- helpdesk/helpdesk/doctype/hd_notification/utils.py
+++ helpdesk/helpdesk/doctype/hd_notification/utils.py
@@ -2,21 +2,24 @@ import frappe
 
 
 @frappe.whitelist()
-def clear(ticket: str | int | None = None, comment: str | None = None):
+def clear(ticket: str | int | None = None, comment: str | None = None, notification: str | None = None):
     """
     Mark notifications as read. No arguments will clear all notifications for `user`.
 
     :param ticket: Ticket to clear notifications for
     :param comment: Comment to clear notifications for
+    :param notification: A single HD Notification name to clear (used for non-ticket notifications)
     """
     filters = {"user_to": frappe.session.user, "read": False}
     if ticket:
         filters["reference_ticket"] = ticket
     if comment:
         filters["reference_comment"] = comment
-    for notification in frappe.get_all(
+    if notification:
+        filters["name"] = notification
+    for notification_name in frappe.get_all(
         "HD Notification", filters=filters, pluck="name"
     ):
         frappe.db.set_value(
-            "HD Notification", notification, "read", 1, update_modified=False
+            "HD Notification", notification_name, "read", 1, update_modified=False
         )
